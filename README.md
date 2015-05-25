e2d3d8845da19e6585fed4daa4d4c59eab7c39c0
Renato Gama <renatoargh@gmail.com>
2015-05-25 16:43:56 -0300

    First commit

--- README.md
+++ README.md
@@ -1,1 +1,66 @@
 # zipper
+Simple compression service for Amazon S3
+---------
+
+Zipper is a HTTP rest API that will allow you to compress files from your Amazon S3 buckets into a `.zip` file.
+
+### Pre-requirements
+
+1. A queue at Amazon SQS service so its easier to scale up
+2. An Amazon EC2 instance (the more RAM it has the more files you'll be able to zip) - currently zipper processes all in memory - this will be changed.
+
+### Installation
+
+1. Clone this repo on your Amazon EC2 instance `git clone https://github.com/gammasoft/zipper.git`
+2. Install dependencies: `cd zipper && npm install`
+3. Create a configuration file like this (replace with your specific values) and put it besides `index.js`:
+```json
+{
+    "queueUrl": "https://sqs.sa-east-1.amazonaws.com/123412341234/yourQueueForZipper",
+    "region": "us-east-1",
+    "accessKeyId": "AKIAJ2E1423G6O67WUAA",
+    "secretAccessKey": "M8sj0opL/GZ8n7Qgak9OC8/81kfLv7ptG7JnZAFM"
+}
+```
+4. Run `node index.js` (or use forever/pm2 to survive hiccups)
+
+### Usage
+
+Send `POST` requests yo your Amazon EC2 instance IP at port `9999` with the follwoing payload:
+
+```json
+{
+  "credentials": {
+    "accessKeyId": "AKIA13222BSG6O67WUAA",
+    "secretAccessKey": "M8kdoopL/GZ8n7Qgak9OC8/81kfLv7ptG7JnZAFM",
+    "region": "sa-east-1"
+  },
+  "keys": [
+    "yourCoolBucket/keyToAFile.xml",
+    "yourCoolBucket/keyToAnotherFile.xml",
+    "yourSecondBucket/theBestFileInTheWorld.pdf",
+  ],
+  "destinationKey": "myThirdBucket/everything.zip"
+}
+```
+
+**Note that** all buckets must be in the same region of your credentials!
+
+**ITS ADVISABLE THAT YOU CREATE SPECIFIC ACCESS CREDENTIALS TO USE WITH ZIPPER. GIVE THEM RESTRICTED ACCESS!**
+
+### Roadmap
+
+1. Manipulate the resulting `.zip` file in disk, which is cheaper than ram.
+2. Implement notifications/hooks (smtp/http)
+3. Implement size filters (e.g. *do not allow resulting files bigger than X, or individual files bigger than Y*)
+
+### Contributions
+
+1. Pull requests are welcome
+2. Open issue tickets whenever you fell its appropriate
+3. Send bitcoin for beer at: **1KP1Fthgkh9TSovkxCMY1wUm3zWpfnuxro**
+
+### License MIT
+
+
+
---
+++ index.js
@@ -0,0 +1,217 @@
+'use strict';
+
+var express = require('express'),
+    bodyParser = require('body-parser'),
+    morgan = require('morgan'),
+    Aws = require('aws-sdk'),
+    async = require('async'),
+    // ZipStream = require('zip-stream'),
+    archiver = require('archiver'),
+    Debug = require('debug'),
+
+    app = express(),
+    debug = new Debug('zipper'),
+    busy = false,
+    env = require('./env.json'),
+    sqs = new Aws.SQS({
+        params: {
+            QueueUrl: env.queueUrl
+        },
+        apiVersion: '2012-11-05',
+        region: env.region,
+        accessKeyId: env.accessKeyId,
+        secretAccessKey: env.secretAccessKey
+    });
+
+app.use(bodyParser.json({
+    limit: '256kb'
+}));
+
+function processJob(job) {
+    busy = true;
+    debug('Processando: %s', job.id);
+
+    var s3client = new Aws.S3({
+        endpoint: 'https://s3-' + job.credentials.region + '.amazonaws.com',
+        s3BucketEndpoint: false,
+
+        accessKeyId: job.credentials.accessKeyId,
+        secretAccessKey: job.credentials.secretAccessKey
+    });
+
+    function getHeads(cb) {
+        debug('Obtendo dados dos arquivos...');
+        debug('Quantidade de arquivos: %s', job.keys.length);
+
+        async.mapSeries(job.keys, function(key, cb) {
+            debug('Obtendo dados do arquivo: %s', key);
+
+            key = key.split('/');
+            s3client.headObject({
+                Bucket: key.shift(),
+                Key: key.join('/')
+            }, cb);
+        }, cb);
+    }
+
+    function checkFiles(heads, cb) {
+        debug('Verificando arquivos...');
+        // TODO: Implement size verification logic
+
+        cb(null, heads);
+    }
+
+    function createZipFile(heads, cb) {
+        if(typeof heads === 'function') {
+            cb = heads;
+            heads = [];
+        }
+
+        cb(null, archiver.create('zip'));
+    }
+
+    function downloadFiles(zip, cb) {
+        zip.on('error', cb);
+
+        async.eachSeries(job.keys, function(key, cb) {
+            debug('Baixando o arquivo: %s', key);
+
+            key = key.split('/');
+            var objectStream = s3client.getObject({
+                Bucket: key.shift(),
+                Key: key.join('/')
+            }).createReadStream();
+
+            zip.append(objectStream, {
+                name: key.join('-')
+            });
+
+            objectStream.on('end', cb);
+        }, function(err) {
+            if(err) {
+                console.log(JSON.stringify(err, null, 4));
+                return cb(err);
+            }
+
+            zip.finalize();
+            cb(null, zip);
+        });
+    }
+
+    function uploadZipFile(zip, cb) {
+        debug('Enviando arquivo: %s', job.destinationKey);
+
+        var key = job.destinationKey.split('/'),
+            upload = s3client.upload({
+                Bucket: key.shift(),
+                Key: key.join('/'),
+                ACL: job.acl || 'private',
+                Body: zip
+            });
+
+        upload.on('httpUploadProgress', debug);
+        upload.send(cb);
+    }
+
+    function sendNotifications(data, cb) {
+        debug('Arquivo disponível em: %s', data.Location);
+
+        if(job.notifications && job.notifications.length) {
+            // TODO: Not Implemented
+            // Enviar job.id e data.Location
+        } else {
+            debug('Nenhuma notificação para enviar');
+        }
+
+        cb();
+    }
+
+    function deleteJob(cb) {
+        debug('Deletando job...');
+
+        sqs.deleteMessage({
+            ReceiptHandle: job.deleteId
+        }, cb);
+    }
+
+    var startTime = new Date();
+    async.waterfall([
+        // getHeads,
+        // checkFiles,
+        createZipFile,
+        downloadFiles,
+        uploadZipFile,
+        sendNotifications,
+        deleteJob
+    ], function(err) {
+        if(err) {
+            throw err;
+        }
+
+        busy = false;
+        debug('Job concluido em: %s segundos', (new Date() - startTime) / 1000);
+        setImmediate(getNext);
+    });
+}
+
+function getNext() {
+    debug('Obtendo jobs...');
+
+    sqs.receiveMessage({
+        AttributeNames: [
+            'ApproximateFirstReceiveTimestamp',
+            'ApproximateReceiveCount'
+        ],
+        MaxNumberOfMessages: 1,
+        VisibilityTimeout: 60,
+        WaitTimeSeconds: 1
+    }, function(err, data) {
+        if(err) {
+            throw err;
+        }
+
+        if(!data.Messages || !data.Messages.length) {
+            debug('Nenhum job...');
+            return setImmediate(getNext);
+        }
+
+        var message = data.Messages[0],
+            job = JSON.parse(message.Body);
+
+        job.id = message.MessageId;
+        job.deleteId = message.ReceiptHandle;
+        processJob(job);
+    });
+}
+
+getNext();
+
+app.post('/', function(req, res, next) {
+    var job = req.body;
+
+    if(!job.credentials || !job.credentials.accessKeyId || !job.credentials.secretAccessKey || !job.credentials.region) {
+        return next(new Error('Credentials missing!'));
+    }
+
+    if(!job.keys || !job.keys.length) {
+        return next(new Error('Keys missing!'));
+    }
+
+    if(!job.destinationKey) {
+        return next(new Error('Destination key missing!'));
+    }
+
+    sqs.sendMessage({
+        MessageBody: JSON.stringify(job)
+    }, function(err, data) {
+        if(err) {
+            return next(err);
+        }
+
+        res.status(202).json({
+            id: data.MessageId
+        });
+    });
+});
+
+app.listen(9999);
